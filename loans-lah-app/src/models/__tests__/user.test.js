c76bd06070f441f80cddd088af7b6a1df0d298e1
Stephan Dowding <sdowding@thoughtworks.com>
2019-09-22 21:36:37 +0800

    password hashing

--- loans-lah-app/src/models/__tests__/user.test.js
+++ loans-lah-app/src/models/__tests__/user.test.js
@@ -16,8 +16,23 @@ afterEach(async () => {
 })
 
 test('saves the user', async () => {
-  const user = await User.create({name: "Alice"})
+  const user = await User.createUser("Alice", "1234")
   const retrieved = await User.findOne({ where: {name: "Alice"} })
   expect(user.id).toBe(retrieved.id)
   expect(retrieved.name).toBe("Alice")
+})
+
+test('password not stored in clear text', async () => {
+  const user = await User.createUser("Alice", "1234")
+  const retrieved = await User.findOne({ where: {name: "Alice"} })
+  expect(user.id).toBe(retrieved.id)
+  expect(retrieved.password).not.toBe("1234")
+})
+
+test('password verified correctly', async () => {
+  const user = await User.createUser("Alice", "1234")
+  const retrieved = await User.findOne({ where: {name: "Alice"} })
+  expect(user.id).toBe(retrieved.id)
+  expect(await retrieved.verifyPassword("abcd")).toBeFalsy()
+  expect(await retrieved.verifyPassword("1234")).toBeTruthy()
 })
--- loans-lah-app/src/models/user.js
+++ loans-lah-app/src/models/user.js
@@ -1,18 +1,36 @@
 import Sequelize from "sequelize"
+import argon2 from "argon2"
 
 const Model = Sequelize.Model;
-export default class User extends Model {}
+export default class User extends Model {
+  static async createUser(name, password){
+    const hashedPassword = await argon2.hash(password, {type: argon2.argon2id})
+    return User.create({
+      name,
+      password: hashedPassword
+    })
+  }
+
+  async verifyPassword(test) {
+    return await argon2.verify(this.password, test)
+  }
+}
 
 export const _init = (sequelize) => {
-    User.init({
+  User.init({
     // attributes
     name: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
     }
-    }, {
+  },
+  {
     sequelize,
     modelName: 'user'
     // options
-    })
+  })
 }
