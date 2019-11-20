47322cf666eb9550142947e8c36032339bb97eb7
Arek Torczuk <torczuk@gmail.com>
2019-11-20 17:07:11 +0800

    Payment - third party dependencies

---
+++ payment/src/main/java/com/thirdparty/payment/Application.java
@@ -0,0 +1,7 @@
+package com.thirdparty.payment;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+}
---
+++ payment/src/test/java/com/thirdparty/payment/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.thirdparty.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+  @Test
+  void shouldStartApp() {
+  }
+}
