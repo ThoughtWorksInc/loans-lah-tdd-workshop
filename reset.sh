f6c5b3bac75aaa6e1e544b1ce7c214cc00b745bc
Arek Torczuk <torczuk@gmail.com>
2019-12-01 21:41:21 +0800

    Fix readme.sh to remove LoanLah images

--- reset.sh
+++ reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 readonly containers=$(docker ps -a | grep loans | awk '{print $1}')
-readonly images=$(docker images -q | grep loan)
+readonly images=$(docker images | grep loans | awk '{print $3}')
 
 if [[ -n "$containers" ]]; then
     docker kill $containers
